f359a10fdaf8cb9876ec51cc897f5dc1e860dd51
George.J <george.j@reflectionsinfos.com>
2021-05-11 17:19:59 +0530

    Simple e-commerce website features.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { ProductListComponent } from './product-list/product-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopBarComponent,
+    ProductListComponent
   ],
   imports: [
     BrowserModule,
+    ReactiveFormsModule,
     AppRoutingModule
   ],
   providers: [],
---
+++ src/app/product-list/product-list.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { products } from '../products';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.sass']
+})
+export class ProductListComponent implements OnInit {
+  products = products;
+  share() {
+    window.alert('The product has been shared!');
+  }
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
